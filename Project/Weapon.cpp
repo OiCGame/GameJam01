d085acc9151623385a226501fd3e2394b28e2bc5
mailaddressforcontact@gmail.com <mailaddressforcontact@gmail.com>
2020-11-28 07:28:08 +0900

    \weapon作成

--- Project/Weapon.cpp
+++ Project/Weapon.cpp
@@ -1,1 +1,25 @@
 #include "Weapon.h"
+
+#include "BulletManager.h"
+#include "Character.h"
+
+
+CWeapon::CWeapon() :
+    m_pOwner(nullptr){
+}
+
+CWeapon::CWeapon(Character* ptr) :
+    m_pOwner(ptr){
+}
+
+CWeapon::~CWeapon() {
+}
+
+void CWeapon::Shot(CVector2 move) {
+    /*
+    CBulletManager::Singleton().Fire(
+        m_pOwner->GetPosition(),
+        move,
+        m_pOwner->GetTeamType());
+    */
+}
--- Project/Weapon.h
+++ Project/Weapon.h
@@ -1,5 +1,34 @@
 #pragma once
+
+
+#include <Mof.h>
+
+
+/// <summary>
+/// 武器
+/// </summary>
 class CWeapon
 {
+private:
+    //! 所有者
+    class Character* m_pOwner;
+public:
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    CWeapon();
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="ptr">所有者</param>
+    CWeapon(class Character* ptr);
+    /// <summary>
+    /// デストラクタ
+    /// </summary>
+    ~CWeapon();
+    /// <summary>
+    /// Bullet発射
+    /// </summary>
+    /// <param name="move">Bulletの位置の更新値</param>
+    void Shot(CVector2 move);
 };
-
