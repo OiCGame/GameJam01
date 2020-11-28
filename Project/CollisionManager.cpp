37eba533799476ad8d6b2667c8edd137ba74ee0a
OiC-b8024-Inoue <49602459+KoreanGinseng@users.noreply.github.com>
2020-11-28 15:50:05 +0900

    Update CollisionManager.cpp

--- Project/CollisionManager.cpp
+++ Project/CollisionManager.cpp
@@ -1,4 +1,7 @@
 #include "CollisionManager.h"
+#include "Character.h"
+#include "Player.h"
+#include "BulletManager.h"
 
 CCollisionManager::CCollisionManager(void)
 {
@@ -10,12 +13,47 @@ CCollisionManager::~CCollisionManager(void)
 
 void CCollisionManager::Update(void)
 {
+	for (auto& enemy : m_pEnemyArray)
+	{
+		//if(m_pPlayer->GetRect())
+
+		for (auto& playerBullet : m_pPlayerBulletArray)
+		{
+			//enemy->;
+			//playerBullet->;
+		}
+	}
 }
 
-void CCollisionManager::Register(std::shared_ptr<CBullet> pBullet)
+void CCollisionManager::Register(const shared_ptr<CBullet>& pBullet, const CollisionLayer& layer)
 {
+	switch (layer)
+	{
+	case CollisionLayer::Player:
+		m_pPlayerBulletArray.push_back(pBullet);
+		break;
+	case CollisionLayer::Enemy:
+		m_pEnemyBulletArray.push_back(pBullet);
+		break;
+	}
+}
+
+void CCollisionManager::Register(const shared_ptr<CCharacter>& pCharacter, const CollisionLayer& layer)
+{
+	switch (layer)
+	{
+	case CollisionLayer::Player:
+		m_pPlayer = dynamic_pointer_cast<CPlayer>(pCharacter);
+		break;
+	case CollisionLayer::Enemy:
+		m_pEnemyArray.push_back(pCharacter);
+		break;
+	}
 }
 
 void CCollisionManager::Release(void)
 {
+	m_pEnemyArray.clear();
+	m_pPlayerBulletArray.clear();
+	m_pPlayerBulletArray.clear();
 }
