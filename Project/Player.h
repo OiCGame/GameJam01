3ac7e660a42bd9abcbc0fce9c163470da34c20e7
OiC_B8186_Seike <B8186@oic.jp>
2020-11-28 13:07:05 +0900

    プレイヤークラスを記述

--- Project/Player.h
+++ Project/Player.h
@@ -1,5 +1,16 @@
 #pragma once
-class CPlayer
+
+#include "Character.h"
+#include "GamePad.h"
+#include "Weapon.h"
+
+class CPlayer : public CCharacter
 {
+private:
+	CWeapon m_Weapon;
+public:
+	CPlayer();
+	~CPlayer();
+	virtual void Update(void) override;
 };
 
