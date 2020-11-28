d584c2f3a92940481a1bf4a38097fdea2a846a7e
OiC-b8024-Inoue <49602459+KoreanGinseng@users.noreply.github.com>
2020-11-28 13:28:45 +0900

    add fuction

---
+++ Project/CollisionManager.cpp
@@ -0,0 +1,21 @@
+#include "CollisionManager.h"
+
+CCollisionManager::CCollisionManager(void)
+{
+}
+
+CCollisionManager::~CCollisionManager(void)
+{
+}
+
+void CCollisionManager::Update(void)
+{
+}
+
+void CCollisionManager::Register(std::shared_ptr<CBullet> pBullet)
+{
+}
+
+void CCollisionManager::Release(void)
+{
+}
--- Project/CollisionManager.h
+++ Project/CollisionManager.h
@@ -2,13 +2,15 @@
 #include "Mof.h"
 
 class CCharacter;
+class CBullet;
 
 class CCollisionManager
 {
 private:
 
-	std::vector<CCharacter*> m_pCharaArray;
+	std::vector<std::shared_ptr<CCharacter>> m_pCharaArray;
 
+	std::vector<std::shared_ptr<CBullet>> m_pBulletArray;
 
 	CCollisionManager(void);
 
@@ -16,5 +18,12 @@ public:
 
 	~CCollisionManager(void);
 
+	void Update(void);
+
+	void Register(std::shared_ptr<CBullet> pBullet);
+
+	void Register(std::shared_ptr<CCharacter> pCharacter);
+
+	void Release(void);
 };
 
