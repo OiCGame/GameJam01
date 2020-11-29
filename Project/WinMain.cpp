17e85e0bf55f23f208cd14af47d3627886b748e6
OiC-b8024-Inoue <49602459+KoreanGinseng@users.noreply.github.com>
2020-11-29 09:17:53 +0900

    Update WinMain.cpp

--- Project/WinMain.cpp
+++ Project/WinMain.cpp
@@ -6,6 +6,8 @@ int WINAPI _tWinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmd
 
 	Mof::LPFramework pFrame = new Mof::CDX11GameFramework(); 
 	Mof::WINDOWSGAMEFRAMEWORKINITIALIZEINFO Info;
+	Info.WindowCreateInfo.Width  = 720;
+	Info.WindowCreateInfo.Height = 960;
 	Info.pApplication = new CGameApp();
 	if ( pFrame->Initialize(&Info) )
 	{
